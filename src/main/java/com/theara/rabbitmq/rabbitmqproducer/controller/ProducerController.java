a3d79c19c54bc9deebe6da8dc5cbb12bce2b02d4
Theara Seng <seng.theara.kh@gmail.com>
2018-03-30 09:52:46 +0700

    create ProducerController

---
+++ src/main/java/com/theara/rabbitmq/rabbitmqproducer/controller/ProducerController.java
@@ -0,0 +1,34 @@
+package com.theara.rabbitmq.rabbitmqproducer.controller;
+
+import com.theara.rabbitmq.rabbitmqproducer.service.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Theara Seng
+ * created on Mar 30, 2018
+ */
+
+@RestController
+@RequestMapping("/producer")
+public class ProducerController {
+
+    private MessageProducer messageProducer;
+
+    @Autowired
+    public ProducerController(MessageProducer messageProducer) {
+        this.messageProducer = messageProducer;
+    }
+
+    @GetMapping
+    public String produce(@RequestParam String message) {
+
+        messageProducer.sendMessage(message);
+
+        return "Message \"" + message + "\" was sent to RabbitMQ";
+    }
+
+}
